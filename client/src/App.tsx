ff4b669a140b302dd00688122a6e3c0aea97adf2
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-05-31 13:03:45 +0330

    frontend-implement navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,24 +3,26 @@ import {
   CssBaseline,
   ThemeProvider,
   createTheme,
-} from "@mui/material";
-import { RouterProvider } from "react-router-dom";
-import router from "./components/Routes";
-import { ApolloProvider } from "@apollo/client";
-import client from "./constants/apollo-client";
-import Guard from "./components/auth/Guard";
+} from '@mui/material'
+import { RouterProvider } from 'react-router-dom'
+import router from './components/Routes'
+import { ApolloProvider } from '@apollo/client'
+import client from './constants/apollo-client'
+import Guard from './components/auth/Guard'
+import Header from './components/header/Header'
 
 const darkTheme = createTheme({
   palette: {
-    mode: "dark",
+    mode: 'dark',
   },
-});
+})
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
+        <Header />
         <Container>
           <Guard>
             <RouterProvider router={router} />
@@ -28,7 +30,7 @@ const App = () => {
         </Container>
       </ThemeProvider>
     </ApolloProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
