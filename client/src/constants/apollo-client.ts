c9407b27ea912f447b3618c39381f5b290da6bec
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-05-31 13:19:08 +0330

    frontend - implement logout

--- client/src/constants/apollo-client.ts
+++ client/src/constants/apollo-client.ts
@@ -1,8 +1,9 @@
-import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
-import { API_URL } from "./urls";
-import excludedRoutes from "./excluded-routes";
-import router from "../components/Routes";
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
+import { API_URL } from './urls'
+import excludedRoutes from './excluded-routes'
+import router from '../components/Routes'
+import { onLogout } from '../utils/logout'
 
 const logoutLink = onError((error) => {
   if (
@@ -10,17 +11,16 @@ const logoutLink = onError((error) => {
     (error.graphQLErrors[0].extensions.originalError as any).statusCode === 401
   ) {
     if (!excludedRoutes.includes(window.location.pathname)) {
-      router.navigate("/login");
-      client.resetStore();
+      onLogout()
     }
   }
-});
+})
 
-const httpLink = new HttpLink({ uri: `${API_URL}/graphql` });
+const httpLink = new HttpLink({ uri: `${API_URL}/graphql` })
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: logoutLink.concat(httpLink),
-});
+})
 
-export default client;
+export default client
