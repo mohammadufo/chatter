fa751f1623b489ed76316b847619840c7f8e69dc
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-03 18:31:59 +0330

    frontend - add gql types

---
+++ client/codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from '@graphql-codegen/cli'
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'http://localhost:3001/graphql',
+  documents: 'src/**/*.ts',
+  generates: {
+    'src/gql/': {
+      preset: 'client',
+      plugins: [],
+    },
+  },
+}
+
+export default config
