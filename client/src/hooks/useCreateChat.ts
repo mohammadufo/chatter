303d58ce0edc09e0f148f1d3555b799913c9f910
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-04 01:20:26 +0330

    frontend - create chat and fetch chat list

--- client/src/hooks/useCreateChat.ts
+++ client/src/hooks/useCreateChat.ts
@@ -1,5 +1,6 @@
 import { useMutation } from '@apollo/client'
 import { graphql } from '../gql'
+import { ChatFragment } from '../fragments/chat.fragment'
 
 const createChatDocument = graphql(`
   mutation CreateChat($createChatInput: CreateChatInput!) {
@@ -14,7 +15,21 @@ const createChatDocument = graphql(`
 `)
 
 const useCreateChat = () => {
-  return useMutation(createChatDocument)
+  return useMutation(createChatDocument, {
+    update(cache, { data }) {
+      cache.modify({
+        fields: {
+          chats(existingChats = []) {
+            const newChatRef = cache.writeFragment({
+              data: data?.createChat,
+              fragment: ChatFragment,
+            })
+            return [...existingChats, newChatRef]
+          },
+        },
+      })
+    },
+  })
 }
 
 export { useCreateChat }
