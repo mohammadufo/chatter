e669cdd1e2f5616989397c3baf4af15a7cf4c966
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-04 16:44:20 +0330

    frontend - create message

--- client/src/hooks/useCreateMessage.ts
+++ client/src/hooks/useCreateMessage.ts
@@ -1,18 +1,36 @@
 import { useMutation } from '@apollo/client'
 import { graphql } from '../gql'
+import { getMessagesDocument } from './useGetMessages'
 
 const createMessageDocument = graphql(`
   mutation CreateMessage($createMessageInput: CreateMessageInput!) {
     createMessage(createMessageInput: $createMessageInput) {
-      _id
-      content
-      createdAt
+      ...MessageFragment
     }
   }
 `)
 
-const useCreateMessage = () => {
-  return useMutation(createMessageDocument)
+const useCreateMessage = (chatId: string) => {
+  return useMutation(createMessageDocument, {
+    update(cache, { data }) {
+      const messagesQueryOptions = {
+        query: getMessagesDocument,
+        variables: {
+          chatId,
+        },
+      }
+      const messages = cache.readQuery({ ...messagesQueryOptions })
+      if (!messages || !data?.createMessage) {
+        return
+      }
+      cache.writeQuery({
+        ...messagesQueryOptions,
+        data: {
+          messages: messages.messages.concat(data?.createMessage),
+        },
+      })
+    },
+  })
 }
 
 export { useCreateMessage }
