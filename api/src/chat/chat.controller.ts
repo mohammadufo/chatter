6bf94c543f80c4b5aba610569b8e279ab3fa6c85
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-09 19:50:39 +0330

    backend - add chat controller

--- api/src/chat/chat.controller.ts
+++ api/src/chat/chat.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { ChatsService } from 'src/chats/chats.service';
 
 @Controller('chat')
-export class ChatController {}
+export class ChatController {
+  constructor(private readonly chatService: ChatsService) {}
+
+  @Get('count')
+  @UseGuards(JwtAuthGuard)
+  async countChats() {
+    return this.chatService.countChats();
+  }
+}
